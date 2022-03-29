a0deb94a8dc49a4227ede1c6e7a503e96d76fe43
Hridyansh <dugar@ualberta.ca>
2022-03-29 02:40:35 -0600

    Function 2

--- operations.py
+++ operations.py
@@ -1,9 +1,8 @@
-import time
-
 from pymongo import MongoClient
 
-port_number = int(input("Enter the port number: "))
-client = MongoClient("mongodb://localhost:"+str(port_number)+"/")
+# port_number = int(input("Enter the port number: "))
+port_number = 27017
+client = MongoClient("mongodb://localhost:" + str(port_number) + "/")
 db = client["291db"]
 
 
@@ -12,7 +11,14 @@ def search_titles():
 
 
 def search_genres():
-    pass
+    # Collection references
+    collection_name = db["name_basics"]
+    collection_title_p = db["title_principals"]
+    collection_title_b = db["title_basics"]
+    collection_title_r = db["title_ratings"]
+
+    genre = input("Enter the genre to search: ")
+    min_count = int(input("Enter the minimum vote count: "))
 
 
 def search_cast():
@@ -27,9 +33,9 @@ def search_cast():
     query = {"primaryName": {"$regex": cast_name, "$options": "i"}}
     docs = collection_name.find(query)
     for doc in docs:
-        print("*"*60)
-        print("Name: "+ doc["primaryName"])
-        print("nconst: "+ doc["nconst"])
+        print("*" * 60)
+        print("Name: " + doc["primaryName"])
+        print("nconst: " + doc["nconst"])
         professions = doc["primaryProfession"]
         print("Professions: ")
         if professions is not None:
@@ -38,15 +44,14 @@ def search_cast():
         else:
             print(professions)
 
-
         # Find the jobs and other details
-        query = {"nconst":doc["nconst"]}
-        job_char = collection_title_p.find(query,{"tconst":1,"job":1,"characters":1,"_id":0})
-        print("-"*60)
+        query = {"nconst": doc["nconst"]}
+        job_char = collection_title_p.find(query, {"tconst": 1, "job": 1, "characters": 1, "_id": 0})
+        print("-" * 60)
 
         for jc in job_char:
-            query = {"tconst":jc["tconst"]}
-            titles = collection_title_b.find(query,{"primaryTitle":1,"_id":0})
+            query = {"tconst": jc["tconst"]}
+            titles = collection_title_b.find(query, {"primaryTitle": 1, "_id": 0})
             printed_flag = False
             for title in titles:
                 if jc["job"] is not None or jc["characters"] is not None:
@@ -63,8 +68,8 @@ def search_cast():
             if not printed_flag:
                 print("No Movie Appearances")
 
-            print("-"*60)
-        print("*"*60)
+            print("-" * 60)
+        print("*" * 60)
 
 
 def add_movie():
@@ -76,16 +81,17 @@ def add_cast():
 
 
 def display_menu():
-    print("*"*60)
+    print("*" * 60)
     print("* {:^56s} *".format("Operations"))
-    print("*"*60)
+    print("*" * 60)
     print("* {:<56s} *".format("1. Search for titles"))
     print("* {:<56s} *".format("2. Search for Genres"))
     print("* {:<56s} *".format("3. Search for Cast Members"))
     print("* {:<56s} *".format("4. Add a Movie"))
     print("* {:<56s} *".format("5. Add a Cast Member"))
     print("* {:<56s} *".format("6. Exit"))
-    print("*"*60)
+    print("*" * 60)
+
 
 def main():
     # Display menu
@@ -94,34 +100,33 @@ def main():
         display_menu()
         choice = int(input("Enter your choice: "))
         if choice == 1:
-            print("*"*60)
+            print("*" * 60)
             print("*{:^58s}*".format("Search for titles"))
-            print("*"*60)
+            print("*" * 60)
             search_titles()
         elif choice == 2:
-            print("*"*60)
+            print("*" * 60)
             print("*{:^58s}*".format("Search for Genres"))
-            print("*"*60)
+            print("*" * 60)
             search_genres()
         elif choice == 3:
-            print("*"*60)
+            print("*" * 60)
             print("*{:^58s}*".format("Search for Cast Members"))
-            print("*"*60)
+            print("*" * 60)
             search_cast()
         elif choice == 4:
-            print("*"*60)
+            print("*" * 60)
             print("*{:^58s}*".format("Add a Movie"))
-            print("*"*60)
+            print("*" * 60)
             add_movie()
         elif choice == 5:
-            print("*"*60)
+            print("*" * 60)
             print("*{:^58s}*".format("Add a Cast Member"))
-            print("*"*60)
+            print("*" * 60)
             add_cast()
         elif choice == 6:
             break
         else:
             print("Invalid Choice")
 
-
-main()
+# main()
